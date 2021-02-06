b59193e0c933ceb02e14c5e1b722fe261ee852f5
synergit <syner.wang@gmail.com>
2021-02-06 17:57:00 -0500

    add textinput for username and password

--- src/screens/SurveyScreen.js
+++ src/screens/SurveyScreen.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 
 const SurveyScreen = () => {
+    const [name, setName] = useState('');
+    const [password, setPwd] = useState('');
+
     return (
         <View>
-            <TextInput style={styles.input}></TextInput>
+            <Text>Enter name: </Text>
+            <TextInput 
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newValue)=> setName(newValue)}
+            />
+            <Text>My name is {name}</Text>
+            <Text>Enter password: </Text>
+            <TextInput
+                secureTextEntry={true} 
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={password}
+                onChangeText={(newValue)=> setPwd(newValue)}
+            />
+            {password.length > 5 ? <Text>Good</Text> : <Text>length of password must be longer than 5 characters</Text>}
         </View>
     );
 };
@@ -14,7 +35,7 @@ const styles = StyleSheet.create({
     input: {
         margin: 15,
         borderColor: 'black',
-        borderWidth: 1
+        borderWidth: 1,
     }
 });
 
